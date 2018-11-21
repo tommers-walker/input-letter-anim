e7440712c31e43bfe76a5c4975ed71c548084da8
Tom Walker <tom.w@niceandserious.com>
2018-11-21 12:44:55 +0000

    Animate in-place working

--- src/javascripts/modules/input.js
+++ src/javascripts/modules/input.js
@@ -1,27 +1,70 @@
 import 'gsap';
+import { utils } from './utils';
 
+const inputForm = document.querySelector('#input-form');
 const userInput = document.querySelector('#input');
 const mirrorInput = document.querySelector('#mirror-input');
 
-const svgSquare = `
-<div class="shape"><svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><path fill="#000" d="M0 0h100v100H0z" fill-rule="evenodd"/></svg></div>`
+const letterAnim = (letter, i) => {
+  const letterTl = new TimelineMax({delay: 0.2*i});
 
-let index = 0;
+  letterTl
+  .to(letter, 0.15, {
+    scaleX: 1.25,
+    scaleY: 0.75,
+    transformOrigin: "50% 100%",
+    repeat: 1,
+    yoyoEase: Power1.easeIn,
+  })
+  .to(letter, 0.1, {
+    y: -150,
+    repeat: 1,
+    yoyoEase: Power1.easeIn,
+  })
+  .to(letter, 0.1, {
+    rotation: 360
+  }, "-=0.15")
+  .to(letter, 0.1, {
+    scaleX: 1.25,
+    scaleY: 0.75,
+    transformOrigin: "50% 100%",
+    repeat: 1,
+    yoyoEase: Power1.easeIn,
+  })
+}
 
 const input = {
   init() {
-    userInput.addEventListener('keyup', function(e) {
-      index++
-      // const charArray = ([...this.value]);
+    userInput.focus();
 
-      // for (let i = 0; i < charArray.length; i++) {
-        // charArray[i]
-        // markup += charArray[i];
-        const letterDiv = `<div id="char${index}" class="char">${e.key}</div>`;
-        mirrorInput.innerHTML += letterDiv;
-        TweenLite.to(`#char${index}`, 1, {opacity:1});
-      // }
+    inputForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+
+      let markup = '';
+      const charArray = [...this.elements["input"].value];
+
+      for (let i = 0; i < charArray.length; i++) {
+        const charMarkup = charArray[i] == " " ? `<div class="char">&nbsp;</div>` : `<div class="char js-char">${charArray[i]}</div>`;
+        markup += charMarkup;
+      }
+
+      mirrorInput.innerHTML = markup;
+      utils.addClass(userInput, 'is-hidden');
+      utils.removeClass(mirrorInput, 'is-hidden');
+
+      const addedEls = Array.from(document.querySelectorAll('.js-char'));
+
+      for (var i = 0; i < addedEls.length; i++) {
+        const letter = addedEls[i];
+        letterAnim(letter, i);
+      }
     });
+
+    mirrorInput.addEventListener('click', function() {
+      utils.removeClass(userInput, 'is-hidden');
+      utils.addClass(mirrorInput, 'is-hidden');
+      userInput.focus();
+    })
   }
 }
 
---
+++ src/javascripts/modules/utils.js
@@ -0,0 +1,20 @@
+const utils = {
+  addClass(el, className) {
+    if (el.classList) {
+      el.classList.add(className);
+    } else {
+      el.className += ' ' + className;
+    }
+  },
+
+  removeClass(el, className) {
+    if (el.classList) {
+      el.classList.remove(className);
+    } else {
+      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+  }
+
+}
+
+export { utils }
